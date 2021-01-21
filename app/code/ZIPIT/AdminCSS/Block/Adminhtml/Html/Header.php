26707a20826d7ec207e8806654ebabe6690c46fb
ZIP-IT <office@1zip-it.com>
2021-01-21 10:45:52 +0200

    Initial Code

---
+++ app/code/ZIPIT/AdminCSS/Block/Adminhtml/Html/Header.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+/**
+ * Custom CSS for AdminHTML
+ * Copyright (C) 2020 Paul Zwagerman
+ * 
+ * This file is part of ZIPIT/AdminCSS.
+ * 
+ * ZIPIT/AdminCSS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace ZIPIT\AdminCSS\Block\Adminhtml\Html;
+
+
+class Header extends \Magento\Backend\Block\Template
+{
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Backend\Block\Template\Context  $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return string
+     */
+    public function GetExtraCSS()
+    {
+        //Your block code
+        return __('Hello Developer! This how to get the storename: %1 and this is the way to build a url: %2', $this->_storeManager->getStore()->getName(), $this->getUrl('contacts'));
+    }
+}
+
